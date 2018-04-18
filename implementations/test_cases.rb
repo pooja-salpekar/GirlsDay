01bc6072715e2e634fcb76cacee91759dbb2dc83
Pooja <psalpekar@babbel.com>
2018-04-18 10:07:29 +0200

    completing the test cases

--- implementations/test_cases.rb
+++ implementations/test_cases.rb
@@ -1,6 +1,7 @@
 require_relative './drawer_implemented.rb'
 require_relative './table_implemented.rb'
 
+puts "Let's build drawers"
 fruits = DrawerImplemented.new
 fruits.insert("apple")
 fruits.insert("banana")
@@ -9,7 +10,7 @@ fruits.print
 puts fruits.find("banana")
 puts fruits.find("random")
 
-
+puts "Let's build tables"
 table = TableImplemented.new
 table.build_drawer("clothes")
 table.build_drawer("fruits")
@@ -20,13 +21,13 @@ table.add_to_drawer("clothes", "jacket")
 table.add_to_drawer("clothes", "skirt")
 table.add_to_drawer("clothes", "scarf")
 
-# multi_drawers.add_to_drawer("fruits", "apple")
-# multi_drawers.add_to_drawer("fruits", "banana")
-# multi_drawers.add_to_drawer("fruits", "peach")
-# multi_drawers.add_to_drawer("fruits", "orange")
+table.add_to_drawer("fruits", "apple")
+table.add_to_drawer("fruits", "banana")
+table.add_to_drawer("fruits", "peach")
+table.add_to_drawer("fruits", "orange")
 
-# multi_drawers.add_to_drawer("devices", "mobile")
-# multi_drawers.add_to_drawer("devices", "tablet")
-# multi_drawers.add_to_drawer("devices", "laptop")
+table.add_to_drawer("devices", "mobile")
+table.add_to_drawer("devices", "tablet")
+table.add_to_drawer("devices", "laptop")
 
 table.print
--- test_cases.rb
+++ test_cases.rb
@@ -1,9 +1,10 @@
 require "./drawer.rb"
 require "./table.rb"
 
+puts "Let's build drawers"
 fruits = Drawer.new
 fruits.print
 
-
+puts "Let's build table"
 table = Table.new
 table.print
